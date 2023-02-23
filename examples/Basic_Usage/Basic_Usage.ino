e128f12b375935892d7438a9c92b4e9338135097
Jonas <stlljonas@gmail.com>
2023-02-23 16:46:13 +0100

    Add clang-format config and format

--- examples/Basic_Usage/Basic_Usage.ino
+++ examples/Basic_Usage/Basic_Usage.ino
@@ -5,22 +5,22 @@ SignalType testSignalType = SignalType::RELATIVE_HUMIDITY_PERCENTAGE;
 int dummyValue = 0;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.print("quantity: ");
-  Serial.println(quantityOf(testSignalType).c_str());
-  Serial.print("unit: ");
-  Serial.println(unitOf(testSignalType).c_str());
+    Serial.begin(115200);
+    Serial.println();
+    Serial.print("quantity: ");
+    Serial.println(quantityOf(testSignalType).c_str());
+    Serial.print("unit: ");
+    Serial.println(unitOf(testSignalType).c_str());
 }
 
 void loop() {
-  Serial.print(quantityOf(testSignalType).c_str());
-  Serial.print(" ");
-  Serial.print(dummyValue);
-  Serial.print(" ");
-  Serial.println(unitOf(testSignalType).c_str());
+    Serial.print(quantityOf(testSignalType).c_str());
+    Serial.print(" ");
+    Serial.print(dummyValue);
+    Serial.print(" ");
+    Serial.println(unitOf(testSignalType).c_str());
 
-  dummyValue += 1;
-  dummyValue = dummyValue % 100;
-  delay(1000); // 1 second
+    dummyValue += 1;
+    dummyValue = dummyValue % 100;
+    delay(1000); // 1 second
 }
