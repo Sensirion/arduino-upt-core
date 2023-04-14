4ba5312284a473a474a4b398beb42974331db3ff
Jonas <stlljonas@gmail.com>
2023-04-14 14:26:23 +0200

    Add DataPoint struct

---
+++ src/DataPoint.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023, Sensirion AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Sensirion AG nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef DATA_POINT_H
+#define DATA_POINT_H
+
+#include "Arduino.h"
+#include "SignalType.h"
+
+struct DataPoint {
+    SignalType signalType = SignalType::UNDEFINED;
+    float value = 0.0f;
+    unsigned long timeStamp = 0;
+    std::string sourceDevice = "UNDEFINED";
+    DataPoint() = default;
+    DataPoint(const SignalType &signalType_, const float &value_,
+              const unsigned long &timeStamp_, const std::string &sourceDevice_)
+        : signalType(signalType_), value(value_), timeStamp(timeStamp_),
+          sourceDevice(sourceDevice_) {}
+};
+
+#endif /* DATA_POINT_H */
