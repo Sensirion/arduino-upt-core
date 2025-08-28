6076f13918cc422c40938de0923b05304649c714
Rolf Laich <rolf.laich@sensirion.com>
2025-08-28 17:01:05 +0200

    Remove unused structs

--- src/BLEGadgetType.h
+++ src/BLEGadgetType.h
@@ -4,32 +4,32 @@
 #include "DeviceType.h"
 
 namespace sensirion::upt::core{
-/// List of supported Sensirion BLE Gadgets
-///
-/// Using a struct allows to access the Device Types 
-/// in the same was as the enum fields
-struct BLEGadgetType{
-    
-    static const DeviceType AQ_MINION(){
-        return DeviceTypeRegistry::CreateBleDeviceType("AQ Minion");
-    };
-    
-    static const DeviceType MYCO2(){
-        return DeviceTypeRegistry::CreateBleDeviceType("MyCO2");
-    }
 
-    static const DeviceType SHT40_GADGET(){
-        return DeviceTypeRegistry::CreateBleDeviceType("SHT40 Gadget");
-    }
-    static const DeviceType SHT43_GADGET(){
-        return DeviceTypeRegistry::CreateBleDeviceType("SHT43 DB");
-    } // SHT43 DB
-    static const DeviceType BLE_DIY_GADGET(){
-        return DeviceTypeRegistry::CreateDeviceType("DIY Gadget", DevicePlatform::BLE, "S");
-    }
-    static const DeviceType HUMI_GADGET(){
-        return DeviceTypeRegistry::CreateBleDeviceType("Smart Humigadget");
-    } // Smart humigadget
+// functions to create the predefined sensor types
+    
+inline const DeviceType AQ_MINION(){
+    return DeviceTypeRegistry::CreateBleDeviceType("AQ Minion");
 };
+
+inline const DeviceType MYCO2(){
+    return DeviceTypeRegistry::CreateBleDeviceType("MyCO2");
+}
+
+inline const DeviceType SHT40_GADGET(){
+    return DeviceTypeRegistry::CreateBleDeviceType("SHT40 Gadget");
+}
+
+inline const DeviceType SHT43_GADGET(){
+    return DeviceTypeRegistry::CreateBleDeviceType("SHT43 DB");
+} // SHT43 DB
+
+inline const DeviceType BLE_DIY_GADGET(){
+    return DeviceTypeRegistry::CreateDeviceType("DIY Gadget", DevicePlatform::BLE, "S");
+}
+
+inline const DeviceType HUMI_GADGET(){
+    return DeviceTypeRegistry::CreateBleDeviceType("Smart Humigadget");
+} // Smart humigadget
+
 } // end namespace
 #endif /* _BLE_GADGET_TYPE_H_ */
