0cb6c321908f683ec6d00eb800a8d5a12793973d
qfisch <quentin.fisch@sensirion.com>
2025-09-15 18:00:36 +0200

    Replaced deviceType example with unit tests

---
+++ test/test_devicetype.cpp
@@ -0,0 +1,59 @@
+#include "DeviceType.h"
+#include "Measurement.h"
+
+#include <unity.h>
+
+using namespace sensirion::upt::core;
+
+void test_ble_device() {
+    DeviceType device_t = BLE_DIY_GADGET();
+    std::string device_label{device_t};
+    DeviceType device_t_bis = DeviceTypeRegistry::GetDeviceType(device_label).value_or(DeviceType("undefined_ble_type"));
+
+    TEST_ASSERT_TRUE(device_t == device_t_bis);
+}
+
+void test_device_alias() {
+    DeviceType ble_device_t = BLE_DIY_GADGET();
+    std::string ble_device_alias("S");
+    DeviceType device_t_from_alias = DeviceTypeRegistry::GetDeviceType(ble_device_alias).value_or(DeviceType("undefined_ble_type"));
+
+    TEST_ASSERT_TRUE(ble_device_t == device_t_from_alias);
+}
+
+void test_get_wired_platform() {
+    DeviceType wired_device_type = SHT4X();
+    DevicePlatform platform = DeviceTypeRegistry::GetDevicePlatform(wired_device_type);
+
+    TEST_ASSERT_TRUE(platform == DevicePlatform::WIRED);
+}
+
+void test_get_ble_platform() {
+    DeviceType ble_device_type = BLE_DIY_GADGET();
+    DevicePlatform platform = DeviceTypeRegistry::GetDevicePlatform(ble_device_type);
+
+    TEST_ASSERT_TRUE(platform == DevicePlatform::BLE);
+}
+
+void test_device_label() {
+    DeviceType wired_device_type = SHT4X();
+    std::string label = deviceLabel(wired_device_type);
+
+    TEST_ASSERT_TRUE(label == "Sht4x");
+}
+
+void setup() {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_ble_device);
+    RUN_TEST(test_device_alias);
+    RUN_TEST(test_get_wired_platform);
+    RUN_TEST(test_get_ble_platform);
+    RUN_TEST(test_device_label);
+
+    UNITY_END();
+}
+
+void loop() {
+    // not used
+}
